71ac13dafe0fa060231610768003ea832da01e2a
Chase Haley <c.haley3399@gmail.com>
2021-07-11 13:31:59 -0500

    Remove unused imports

--- src/components/ListingContainer/ListingContainer.js
+++ src/components/ListingContainer/ListingContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import ListingCard from '../ListingCard/ListingCard';
 import ListingContainerStyles from './ListingContainer.styled';
 import listings from '../../data/listings';
